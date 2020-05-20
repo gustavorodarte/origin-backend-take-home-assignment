b766b9e42a8c697559620e8d5b938931a10c502f
Gustavo Rodarte <gustavo.rodarte@zrp.com.br>
2020-05-19 21:28:07 -0300

    Added feature test for calculate-risk endpoint

---
+++ specs/features/api/calculateInsurenceRisk/calculateInsuranceRisk.spec.js
@@ -0,0 +1,27 @@
+const request = require('specs/support/request');
+
+describe('API :: POST :: /api/insurances/calculate-risk', () => {
+  describe('When pass the correct payload', () => {
+    test('return success with correct risk calculation', async () => {
+      const { body } = await request()
+        .post('/api/insurances/calculate-risk')
+        .send({
+          age: 35,
+          dependents: 2,
+          house: { ownership_status: 'owned' },
+          income: 0,
+          marital_status: 'married',
+          risk_questions: [0, 1, 0],
+          vehicle: { year: 2018 },
+        })
+        .expect('200');
+
+      expect(body).toEqual({
+        auto: 'regular',
+        disability: 'ineligible',
+        home: 'economic',
+        life: 'regular',
+      });
+    });
+  });
+});
